0960391369eaf4f112797032fa85633e1afa6a03
mehmetCelenk <celenkm01@gmail.com>
2026-05-15 18:39:33 +0300

    Fix macOS media device access

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,13 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let builder = tauri::Builder::default()
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_updater::Builder::new().build());
+
+    #[cfg(target_os = "macos")]
+    let builder = builder.plugin(tauri_plugin_localhost::Builder::new(1421).build());
+
+    builder
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
